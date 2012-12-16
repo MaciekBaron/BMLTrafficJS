79a2f7a0be981c257d5216955d4806928b8c2917
Maciek Baron <thebezet@gmail.com>
2012-12-16 15:57:20 +0000

    Adding comments and width an height support
    Adding comments to the code and width an height support instead of using
    a square of size n

--- js/core.js
+++ js/core.js
@@ -1,51 +1,66 @@
-var BML = function (n, mainArray, rule, colors) {
-	this.n = n;
+var BML = function (width, height, mainArray, rule, colors) {
+	// Size of the visualisation
+	this.width = width;
+	this.height = height;
 
+	// The array with initial pixels
 	this.mainArray = mainArray;
 
-	this.supportArray = new Array(n);
-	for (var i = 0; i < n; i++) {
-		this.supportArray[i] = new Array(n);
+	// An auxiliary array used in our step function
+	this.supportArray = new Array(width);
+
+	for (var i = 0; i < width; i++) {
+		this.supportArray[i] = new Array(height);
 	}
 
-	this.State = {
-		EMPTY: 0,
-		RED: 1,
-		BLUE: 2
-	}
-
+	// Our rule (which should be an array containing 27 digits)
 	this.rule = rule;
+
+	// Our colors object containing the first and the second color
 	this.colors = colors;
 };
 
 BML.prototype = {
+	/*
+		This function is the main step function. You can ask it to perform
+		a certain number of steps at a time.
+	*/
 	move_step: function(number) {
 		
-		for (var i = 0; i < number; i++) {
-			for (var x = 0; x < this.n; x++) {
-				for (var y = 0; y < this.n; y++) {
-					this.supportArray[x][y] = this.rule[this.mainArray[(x+1)%n][y] + 3*this.mainArray[x][y] + 9*this.mainArray[(x-1+n)%n][y]];
+		while (number > 0) {
+			for (var x = 0; x < this.width; x++) {
+				for (var y = 0; y < this.height; y++) {
+					// We apply our rule, first we move the "blue cars"
+					this.supportArray[x][y] = this.rule[this.mainArray[(x+1)%this.width][y] + 3*this.mainArray[x][y] + 9*this.mainArray[(x-1+this.width)%this.width][y]];
 				}
 			}
 
-			for (var x = 0; x < this.n; x++) {
-				for (var y = 0; y < this.n; y++) {
-					this.mainArray[x][y] = this.rule[this.supportArray[x][(y+1)%n] + 3*this.supportArray[x][y] + 9*this.supportArray[x][(y-1+n)%n]];
+			for (var x = 0; x < this.width; x++) {
+				for (var y = 0; y < this.height; y++) {
+					// Now we move our "red cars"
+					this.mainArray[x][y] = this.rule[this.supportArray[x][(y+1)%this.height] + 3*this.supportArray[x][y] + 9*this.supportArray[x][(y-1+this.height)%this.height]];
 				}
 			}
+			number--;
 		}
 	},
-
+	/*
+		This is the rendering function. We simply generate image data based
+		on the calculated values.
+	*/
 	render: function(context, canvasWidth, canvasHeight) {
+		// Get canvas data so that we can create image data
 		var canvasData = context.getImageData(0, 0, canvasWidth, canvasHeight);
 
-		for (var x = 0; x < this.n; x++) {
-			for (var y = 0; y < this.n; y++) {
+		for (var x = 0; x < this.width; x++) {
+			for (var y = 0; y < this.height; y++) {
+
 				var index = (x + y * canvasWidth) * 4;
-				canvasData.data[index    ] = (this.mainArray[x][y] == this.State.RED) ? this.colors.first[0] : this.colors.second[0]; // Red
-				canvasData.data[index + 1] = (this.mainArray[x][y] == this.State.BLUE) ? this.colors.second[1] : this.colors.first[1];; // Green
-				canvasData.data[index + 2] = (this.mainArray[x][y] == this.State.BLUE) ? this.colors.second[2] : this.colors.first[2]; // Blue
-				canvasData.data[index + 3] = (this.mainArray[x][y] == this.State.EMPTY) ? 0 : 255; //Alpha
+				// Data is defined by 4 byte "chunks", the first three are red, green and blue values, the last one is the alpha value
+				canvasData.data[index    ] = (this.mainArray[x][y] == 1) ? this.colors.first[0] : this.colors.second[0]; // Red
+				canvasData.data[index + 1] = (this.mainArray[x][y] == 2) ? this.colors.second[1] : this.colors.first[1];; // Green
+				canvasData.data[index + 2] = (this.mainArray[x][y] == 2) ? this.colors.second[2] : this.colors.first[2]; // Blue
+				canvasData.data[index + 3] = (this.mainArray[x][y] == 0) ? 0 : 255; // Alpha
 			}
 		}
 
